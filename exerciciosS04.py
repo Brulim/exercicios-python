21851d1cfe0749d672306477c46a67347a1e73d6
Bruno Lima <arquitetobrunolima@gmail.com>
2022-06-14 13:22:34 -0300

    ex 1 e 2

---
+++ exerciciosS04.py
@@ -0,0 +1,21 @@
+"""
+EXERCÍCIOS SEÇÃO 4 - Varíáveis e Tipos de Dados em Python
+
+1 - Faça um programa que leia um número inteiro e o imprima:
+
+num = 5
+print(num)
+
+2 - Ler e imprimir um número real:
+
+# type = float (lembrar de utilizar ponto e não vírgula)
+
+num = '2.5'
+print(num)
+
+3 - Peça para o usuário digitar três valores inteiros e imprima a soma deles:
+
+"""
+
+num = '2.5'
+print(num)
