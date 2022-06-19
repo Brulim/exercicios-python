4a7296ba4826c5895f94bf7eb64c7198c1f8fda4
Bruno Lima <arquitetobrunolima@gmail.com>
2022-06-19 11:57:50 -0300

    S07 Dicionários = Mapas

--- S07_Dicionarios.py
+++ S07_Dicionarios.py
@@ -101,11 +101,48 @@ novo = receita.copy()
 #Forma 2 # Shallow Copy (espelhados)
 novo = receita
 
-"""
+# Mapas = dicionários
+
+# Iterar sobre dicionários
+
 receita = {'jan': 100, 'fev': 120, 'mar': 300}
-novo = receita
-print(novo)
-novo['d'] = 4
 
 print(receita)
-print(novo)
+
+for chave in receita:
+    print(chave)
+
+for chave in receita:
+    print(receita[chave])
+
+for chave in receita:
+    print(f'Em {chave} recebi R${receita[chave]}')
+
+for valor in receita.values():
+    print(valor)
+
+# Desempacotamento de dicionários
+
+print(receita)
+
+print(receita.items())
+
+for chave, valor in receita.items():
+    print(f'chave={chave} e valor={valor}')
+
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita.values()))
+
+"""
+receita = {'jan': 100, 'fev': 120, 'mar': 300}
+
+print(receita)
+
+print(receita.items())
+
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita.values()))
